254cb06ef14fd2d048d1c7d1d528b36043c2d7e7
Kilian Tyler <kilian@kil.dev>
2025-11-10 18:06:07 -0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  typedRoutes: true,
+  reactCompiler: true,
+  cacheComponents: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/vibecheck/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/vibecheck/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
+
+export default config;
